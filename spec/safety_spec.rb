51292638ad8bd96ed10a547a2f6ed7e30e11bfd5
Jeroen van Dijk <jeroen@jeevidee.nl>
2011-02-26 20:22:23 +0100

    Set some specifications

--- spec/safety_spec.rb
+++ spec/safety_spec.rb
@@ -3,18 +3,40 @@ require 'spec_helper'
 describe "Travis VM" do
   
   context "restrictions" do
-    it "should not allow to connect with the internet"
+    it "should not allow to connect with the internet" do
+      lambda {
+        require 'net/http'
+        Net::HTTP.get_print 'www.example.com', '/index.html'
+      }.should raise_error
+    end
+    
     it "should not take more than X minutes to execute a test suite"
     it "should not be allowed to do anything outside of the users home dir"
   end
 
   context "runner's workspace" do
-    it "should be restored after use"
+    it "should be restored after use" do
+      pending
+      # Create files
+      # Run clean up
+      # Files should not exist anymore
+    end
+    
+    it "should contain rvm directory" do
+      pending
+    end
   end
   
   context "runner's processes" do
-    it "should be cleaned up after a build"
-    it "should not have access to resources outside of the home dir"
+    it "should be cleaned up after a build" do
+      pending
+      # Create processes
+      # Run clean up
+      # Processes should not exist anymore
+    end
+    it "should not have access to resources outside of the home dir" do
+      lambda { Dir.chdir("/home/vagrant") }.should raise_error LoadError
+    end
   end
   
   context "services" do
